363e9fd1ed0e071901567aa7cbc6fd88c930a441
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-04-12 12:26:21 -0300

    finalizando form validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+
+// Owl Carousel
 $(document).ready(function() {
 
     $('.owl-carousel').owlCarousel();
@@ -5,3 +7,36 @@ $(document).ready(function() {
     $('.featured-item a').addClass('btn btn-light stretch-link');
     
 })
+
+// Form Validate
+var form = document.getElementById('form');
+form.onsubmit = function() {
+    let name = document.getElementById('name').value;
+    if(name == "") {
+        document.getElementById('messageNome').innerHTML = "Informe seu nome"
+        return false
+    } else {
+        document.getElementById('messageNome').innerHTML = ""
+    }
+}
+function validaEmail(elemento){
+
+    elemento.addEventListener('focusout', function() {
+
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+
+        if(this.value.match(emailValido)){
+            document.querySelector('#messageEmail').innerHTML = "";
+            document.querySelector('#btn').disabled = false;
+            return false;
+        } else {
+            document.querySelector('#messageEmail').innerHTML = "Padrão incorreto";
+            document.querySelector('#btn').disabled = true;
+        };
+  
+    });
+  }
+  let campoEmail = document.querySelectorAll('input#email');
+  for( let emFoco of campoEmail) {
+      validaEmail(emFoco);
+  }
